f80f69b1dce0a2dc54d9329201c48e6bd3873a49
pzoladz <zdjjeden@gmail.com>
2019-11-01 17:27:57 +0100

    Adding db

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-from flask import Flask
+import helper
+from flask import Flask, request, Response
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World!\n'
+
+@app.route('/item/new', methods=['POST'])
+def add_item():
+    # Get item from the POST body
+    req_data = request.get_json()
+    item = req_data['item']
+
+    # Add item to the list
+    res_data = helper.add_to_list(item)
+
+    # Return error if item not added
+    if res_data is None:
+        response = Response("{'error'}: 'Item not added -" + item + "'}", status=400, mimetype='application.json')
+        return response
+
+    # Return response
+    response = Response(json.dumps(res_data), mmietype='application/json')
+
+    return response
